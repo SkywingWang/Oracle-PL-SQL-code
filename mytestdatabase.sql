24d454edf355881caa13646b5852b9800407ed42
SkywingWang <wangty9153@126.com>
2017-04-16 11:07:29 +0800

    Add files via upload
    update

--- mytestdatabase.sql
+++ mytestdatabase.sql
@@ -69,8 +69,35 @@ select order_num FROM orderitems WHERE prod_id = 'TNT2';
 SELECT cust_id FROM orders WHERE order_num IN (20005,20007);
 SELECT cust_name, cust_contact FROM customers WHERE cust_id IN (10001,10004);
 SELECT cust_name, cust_contact FROM customers WHERE cust_id IN (SELECT cust_id FROM orders WHERE order_num IN (SELECT order_num FROM orderitems WHERE prod_id = 'TNT2'));
-SELECT COUNT(*) AS orders FROM orders WHERE cust_id = 10001;
+SELECT COUNT(*) AS orders FROM orders WHERE cust_id = 10001; 
 SELECT cust_name,cust_state,(SELECT COUNT(*) FROM orders WHERE orders.cust_id = customers.cust_id) AS orders FROM customers ORDER BY cust_name;
 select vend_name,trim(prod_name),prod_price FROM vendors,products WHERE vendors.vend_id = products.vend_id ORDER BY vend_name, prod_name;
-SELECT vend_name,prod_name,prod_price FROM vendors INNER JOIN products ON vendors.vend_id = products.vend_id;
 SELECT prod_name,vend_name,prod_price,quantity FROM orderitems,products,vendors WHERE products.vend_id = vendors.vend_id and orderitems.prod_id= products.prod_id and order_num = 20005;
+SELECT cust_name,cust_contact FROM customers WHERE cust_id IN (SELECT cust_id FROM orders WHERE order_num IN (SELECT order_num FROM orderitems WHERE prod_id = 'TNT2'));
+SELECT cust_name,cust_contact FROM customers,orders,orderitems WHERE customers.cust_id = orders.cust_id AND orderitems.order_num = orders.order_num AND prod_id = 'TNT2'; 
+SELECT cust_name,cust_contact FROM customers INNER JOIN orders ON customers.cust_id = orders.cust_id INNER JOIN orderitems ON orderitems.order_num = orders.order_num WHERE prod_id = 'TNT2';
+SELECT prod_id,prod_name FROM products WHERE vend_id = (SELECT vend_id FROM products WHERE prod_id = 'DTNTR');
+SELECT p1.prod_id,p1.prod_name FROM products p1 INNER JOIN products p2 on p1.vend_id = p2.vend_id where p2.prod_id = 'DTNTR';
+SELECT customers.cust_id,orders.order_num FROM customers INNER JOIN orders ON customers.cust_id = orders.cust_id;
+SELECT customers.cust_id,orders.order_num FROM customers LEFT OUTER JOIN orders ON customers.cust_id = orders.cust_id;
+SELECT customers.cust_id,orders.order_num FROM customers RIGHT OUTER JOIN orders ON orders.cust_id = customers.cust_id;
+SELECT customers.cust_name, COUNT(orders.order_num) AS num_ord FROM customers INNER JOIN orders ON customers.cust_id = orders.cust_id group by customers.cust_name;
+SELECT customers.cust_name,COUNT(orders.order_num) AS num_ord FROM customers LEFT OUTER JOIN orders ON customers.cust_id = orders.cust_id GROUP BY customers.cust_name;
+SELECT vend_id,prod_id,prod_price FROM products WHERE prod_price <= 5;
+SELECT vend_id,prod_id,prod_price FROM products WHERE prod_price <= 5 UNION SELECT vend_id,prod_id,prod_price FROM products WHERE vend_id IN (1001,1002);
+INSERT INTO Customers(cust_id,cust_name,cust_address,cust_city,cust_state,cust_zip,cust_country) VALUES(10006,'Pep E. LaPew','100 Main Stret','Los Angeles','CA','90046','USA');
+UPDATE customers SET cust_email = 'elmer@fudd.com' WHERE cust_id = 10005;
+UPDATE customers SET cust_name = 'The Fudds',cust_email = 'elmer@fudd.com' WHERE cust_id = 10005;
+SELECT cust_name,cust_contact FROM customers,orders,orderitems WHERE customers.cust_id = orders.cust_id AND orderitems.order_num = orders.order_num AND prod_id = 'TNT2';
+CREATE VIEW productcustomers AS SELECT cust_name,cust_contact,prod_id FROM customers,orders,orderitems WHERE customers.cust_id = orders.cust_id AND orderitems.order_num = orders.order_num;
+SELECT cust_name,cust_contact FROM productcustomers WHERE prod_id = 'TNT2';
+CREATE OR REPLACE PROCEDURE Hello IS BEGIN
+  DBMS_OUTPUT.PUT_LINE('Hello World');
+END;
+DECLARE 
+  CURSOR c_vendors IS 
+  SELECT vend_id,vend_name,vend_address,vend_city,vend_state,vend_zip,vend_country FROM vendors;
+BEGIN
+  OPEN c_vendors;
+  CLOSE c_vendors;
+END;
